c747e69f1c47e638fe6bc4e9e92954651c10221a
perymerdeka <tkjasikk@gmail.com>
2021-12-02 00:16:25 +0700

    Adding Blog API documentation UI

--- core/settings/base.py
+++ core/settings/base.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
 # Default primary key field type
