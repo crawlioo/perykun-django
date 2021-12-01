25509b95a4cdaf1a4870a715ed617bd0f99626d0
perymerdeka <tkjasikk@gmail.com>
2021-12-02 00:40:11 +0700

    Cors Fix

--- core/settings/base.py
+++ core/settings/base.py
@@ -143,6 +143,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# CORS
+CORS_ORIGIN_ALLOW_ALL = True
+
 # REST FRAMEWORK CONFIG
 
 
