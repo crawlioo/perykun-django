73229f38697cc6f6ff7ebc8ff83b79bd36c7cae6
perymerdeka <tkjasikk@gmail.com>
2021-12-04 00:00:08 +0700

    add datas

--- core/settings/staging.py
+++ core/settings/staging.py
@@ -1,9 +1,9 @@
 # Staging Server Settings
 from .base import *
 
-ALLOWED_HOSTS = ['damp-reaches-35001.herokuapp.com']
+ALLOWED_HOSTS = ["damp-reaches-35001.herokuapp.com"]
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ["SECRET_KEY"]
 DEBUG = False
 
 # middlewire third party
@@ -19,14 +19,13 @@ DATABASES = {
         "USER": "mrfqbfccfmhihz",
         "PASSWORD": "dc16a413949697ece35873b55f81b61747f34633e9478bc681a13fd41bfcd746",
         "HOST": "ec2-34-194-119-178.compute-1.amazonaws.com",
-        "PORT": "5432"
+        "PORT": "5432",
     }
 }
 
 
 # REST FRAMEWORK CONFIG
-REST_FRAMEWORK ** {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    )
+REST_FRAMEWORK = {
+    **REST_FRAMEWORK,
+    **{"DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",)},
 }
